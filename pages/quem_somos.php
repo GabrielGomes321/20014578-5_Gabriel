6d42c040238bcb35a3613cb1128153d4a0ceac59
Gabriel Gomes <gabrielgomesif@hotmail.com>
2021-08-27 00:09:12 -0300

    estrutura

--- pages/quem_somos.php
+++ pages/quem_somos.php
@@ -1,19 +1,19 @@
 <div class="prod-cl">
         <img src="images/who_im.jpg">
-        <div class="legend">Maria Salvador</div>
+        <div class="legend">Ana Machado</div>
 </div>
 <div class="prod-cr">
     <h1>Quem somos</h1>
     <p>
-        Caramelo Doces Artesanais é o resultado de um sonho. Um dia, uma menina apaixonada por confeitaria e doçaria saiu do interior de Minas para correr atrás de seus objetivos.
+        Delícia em pedaços é o resultado de um sonho. Um dia, uma menina apaixonada por doces caseiros determinada a crescer.
     </p>
     <p>
-        Essa menina cresceu e aos 25 se especializou em confeitaria francesa pela Lenôtre em Paris.
+       Essa garota criada em um sitio ficou famosa por seus deliciosos doces caseiros.
     </p>
     <p>
-        Hoje, de volta ao Brasil, juntamente com seu marido, a menina crescida, mas com a alma doce como de criança, dedica-se de corpo e alma a produzir doces artesanais inspirados na patisserie francesa.
+        Hoje, juntamente com seu marido, a menina crescida, mas com a alma doce como de criança, dedica-se de corpo e alma a produzir doces artesanais passandos de geração em geração.
     </p>
     <p>
-        Prazer, sou Maria, a menina que nunca deixou de sonhar.
+        Prazer, sou Ana, a menina que nunca deixou de sonhar.
     </p>
 </div>
