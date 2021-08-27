8fdd12db9107dc69a85a000c94f67f6b01df8c47
Gabriel Gomes <gabrielgomesif@hotmail.com>
2021-08-26 22:50:26 -0300

    estrutura

---
+++ dados/produtos.php
@@ -0,0 +1,27 @@
+<?php
+
+$produtos['Musi'] = array(
+    'nome' => 'Musi de maracuja',
+    'descricao' => 'musi de maracuja uma delicia.
+    <br/><br/>
+    Ideal para ocasiões em que o requinte é fundamental.
+    <br/><br/>
+    Pedido mínimo de 3 unidades.',
+    'preco' => 12,00
+    'imagem' => 'musi.jpg'
+);
+
+$produtos['Cocada'] = array(
+    'nome' => 'Cocada doce de coco',
+    'descricao' => 'cocada ideial para todas as ocasiões',
+    'preco' => 4.5,
+    'imagem' => 'cocada.jpg'
+);
+
+$produtos['goiabada'] = array(
+    'nome' => 'Goiabada doce delicioso de goiaba',
+    'descricao' => 'um doce como você nunca experimentou. Cuidadosamente preparada com goiabas selecionadas.
+    <br/>Serve 6 pessoas.',
+    'preco' => 6.7,
+    'imagem' => 'goiabada.jpg'
+);
